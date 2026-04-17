2e0c391ef5c5d5582e85b0e262e44b39634a03b0
Hello2345de <brayden.fu@gmail.com>
2026-04-17 22:26:05 +0800

    Create config.py

---
+++ task1/config.py
@@ -0,0 +1,5 @@
+GEMINI_API_KEYS = ["AIzaSyChfvIu8NWl4RTe-B5Y-pm7F8E4Dbz0pH4","AIzaSyAIAdXfGE2hf9PpeUY8zSvj1012tE9fABA",
+                  "AIzaSyCDdXVpsDfvl69lZXE9HlyCoyRyGgH-Ij4", "AIzaSyA5ihdSpLmqbwCqpZRoPmavPn7VVQhGmPY", "AIzaSyBdib7hY0itUmqQRT8SH57_QBCp67BlPks"]
+
+
+NVIDIA_API_KEY = "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"
